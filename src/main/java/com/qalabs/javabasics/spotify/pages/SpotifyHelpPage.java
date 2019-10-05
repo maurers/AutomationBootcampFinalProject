3f71ef3162a6f3898376aa38e1c17e27007f2683
ArCiGo <armandocifuentes_1@hotmail.com>
2019-10-05 11:28:25 -0500

    Update of isBoolean() method

--- src/main/java/com/qalabs/javabasics/spotify/pages/SpotifyHelpPage.java
+++ src/main/java/com/qalabs/javabasics/spotify/pages/SpotifyHelpPage.java
@@ -4,22 +4,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-
 import utils.Log;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+
 public class SpotifyHelpPage extends BasePage {
+
+    // Attributes
+
+    private WebDriverWait wait;
+
+    public final static String BASE_URL = "https://support.spotify.com/mx/?_ga=2.47006823.1304134064.1570227736-386534741.1570227736";
+
+    // Elements
+
     @FindBy(how = How.ID, using = "nav-link-help")
     private WebElement spHelpElement;
 
     @FindBy(how = How.XPATH, using = "//div[contains(@class, 'navbar-logo')]/a[@href='http://www.spotify.com/mx/?_ga=2.20450680.1304134064.1570227736-386534741.1570227736']")
     private WebElement spIconLogo;
 
-    private WebDriverWait wait;
-    public final static String BASE_URL = "https://support.spotify.com/mx/?_ga=2.47006823.1304134064.1570227736-386534741.1570227736";
+
+    // Constructor
 
     public SpotifyHelpPage(WebDriver driver) {
         super(driver);
@@ -27,12 +34,16 @@ public class SpotifyHelpPage extends BasePage {
         wait = new WebDriverWait(driver, 5);
     }
 
+    // Actions
+
     public boolean isLoaded() {
+        wait.until(elementToBeClickable(spHelpElement));
+
         try {
-            wait.until(elementToBeClickable(spHelpElement));
-            return true;
-        } catch(RuntimeException exception) {
-            System.out.println("Spotify Help page is not loaded");
+            return this.spHelpElement.isDisplayed();
+        } catch(RuntimeException ex) {
+            Log.error(ex.toString());
+
             return false;
         }
     }
@@ -41,6 +52,7 @@ public class SpotifyHelpPage extends BasePage {
     public SpotifyHomePage clickOnSpotifyIcon() {
         if(spIconLogo.isDisplayed()) {
             spIconLogo.click();
+
             return new SpotifyHomePage(driver);
         }
         return null;
