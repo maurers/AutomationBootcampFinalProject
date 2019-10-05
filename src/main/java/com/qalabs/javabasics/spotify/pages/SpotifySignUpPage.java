34d333e697d32421f8614cedae662328bc69519b
SigifredoPJ <sigifredopj@gmail.com>
2019-10-04 21:48:42 -0500

    JavierSignupPage

--- src/main/java/com/qalabs/javabasics/spotify/pages/SpotifySignUpPage.java
+++ src/main/java/com/qalabs/javabasics/spotify/pages/SpotifySignUpPage.java
@@ -2,6 +2,9 @@ package com.qalabs.javabasics.spotify.pages;
 
 import com.qalabs.javabasics.spotify.components.signup.SignUpComponent;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SpotifySignUpPage extends BasePage {
@@ -22,4 +25,52 @@ public class SpotifySignUpPage extends BasePage {
     public SpotifyHomePage clickOnSpotifyIcon() {
         return null;
     }
+
+    @FindBy(how = How.ID, using = "register-email")
+    private WebElement emailInput;
+
+    @FindBy(how = How.ID, using = "register-confirm-email")
+    private WebElement confirmEmailInput;
+
+    @FindBy(how = How.ID, using = "register-password")
+    private WebElement passInput;
+
+    @FindBy(how = How.ID, using = "register-displayname")
+    private WebElement nameInput;
+
+    @FindBy(how = How.ID, using = "register-dob-month")
+    private WebElement monthDropdown;
+
+    @FindBy(how = How.ID, using = "")
+    private WebElement monthSelect;
+
+    @FindBy(how = How.ID, using = "register-dob-day")
+    private WebElement dayInput;
+
+    @FindBy(how = How.ID, using = "register-dob-year")
+    private WebElement yearInput;
+
+    @FindBy(how = How.ID, using = "")
+    private WebElement genderRadioButtons;
+
+    @FindBy(how = How.ID, using = "register-thirdparty")
+    private WebElement ShareCheckBox;
+
+    @FindBy(how = How.ID, using = "register-button-email-submit")
+    private WebElement signUpButton;
+
+
+
+    public SpotifySignUpPage emailInput(String email) {
+
+        this.emailInput.sendKeys(email);
+        return null;
+    }
+
+    public SpotifySignUpPage confirmEmailInput(String email){
+        this.confirmEmailInput.sendKeys(email);
+        return null;
+    }
+    
+
 }
